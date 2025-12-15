8943df449dcb9841405e9d3441a780427d0cc2bc
Amit-9006876 <amitsingh9006876@gmail.com>
2025-12-15 22:52:47 +0530

    Clean project and prepare for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(() => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/DevUpdatePro/",
-}));
+  base: "/GitHubUserInfo/",
+});
